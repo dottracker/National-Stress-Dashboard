f78d833419d632c4e2ed13faf3b108cbbb829f9f
dottracker <jollybearpro@gmail.com>
2026-02-20 20:12:40 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -381,7 +381,7 @@ export default function Dashboard({ initialData, lastUpdated }) {
           100% { transform: translateX(-50%); }
         }
         .animate-marquee {
-          animation: marquee 40s linear infinite;
+          animation: marquee 120s linear infinite;
         }
         .custom-scrollbar::-webkit-scrollbar {
           width: 4px;

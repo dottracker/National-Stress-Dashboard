16076a1af47455c359ad326ed62b1a2308cead00
dottracker <jollybearpro@gmail.com>
2026-08-02 10:18:25 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="en" className="dark">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700;800&display=swap" rel="stylesheet" />
+      </Head>
+      <body className="bg-[#020617] text-slate-100 font-mono antialiased selection:bg-cyan-500 selection:text-black">
         <Main />
         <NextScript />
       </body>
